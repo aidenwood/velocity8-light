fa9f73a51380f9cba920d32f50b7a60a1a7fbae4
Aiden Wood <aidenthomasgodbywood@gmail.com>
2023-08-27 11:10:35 +1000

    aaaa

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,14 @@ import { defineConfig } from 'astro/config';
 import partytown from "@astrojs/partytown";
 import purgecss from "astro-purgecss";
 import tailwind from "@astrojs/tailwind";
-
 import criticalCss from "astro-critical-css";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [partytown(), purgecss(), tailwind(), criticalCss()]
+  integrations: [partytown(), purgecss(), tailwind(), criticalCss(), sitemap()],
+  experimental: {
+    viewTransitions: true
+   }
 });
